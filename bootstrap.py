46a67821fe8e0c7d7415d0ab7d19e98886a0840c
LorenzoPFLD <55234946+LorenzoPFLD@users.noreply.github.com>
2021-04-12 16:56:44 +0200

    Update bootstrap.py
    monte carlo algorithm

--- bootstrap.py
+++ bootstrap.py
@@ -1,26 +1,32 @@
-import cv2
+import numpy as np
+from pymc3 import *
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+size = 200
+true_intercept = 1
+true_slope = 2
+x = np.linspace(0, 1, size)
+# y = a + b*x
+true_regression_line = true_intercept + true_slope * x
+# add noise
+model1 = true_regression_line + np.random.normal(scale=.5, size=size) #Noisy
+model2 = true_regression_line + np.random.normal(scale=.2, size=size) #Less Noisy
 
-faceCascade = cv2.CascadeClassifier(cascPath)
+data = dict(x1=model1, x2=model2, y=true_regression_line)
+with Model() as model:
+    # specify glm and pass in data. The resulting linear model, its likelihood and 
+    # and all its parameters are automatically added to our model.
+    glm.glm('y ~ x1 + x2', data)
+    step = NUTS() # Instantiate MCMC sampling algorithm
+    trace = sample(2000, step, progressbar=False)
+    
+plt.figure(figsize=(7, 7))
+traceplot(trace)
+plt.tight_layout();
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-
-print "Found {0} faces!".format(len(faces))
-
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+print('Model 1:',mean_absolute_error(true_regression_line,model1))
+print('Model 2:', mean_absolute_error(true_regression_line,model2))
+print('Average:',mean_absolute_error(true_regression_line,model1*.5+model2*.5))
+print('MCMC:',mean_absolute_error(true_regression_line,intercept+x1param*model1+x2param*model2))
